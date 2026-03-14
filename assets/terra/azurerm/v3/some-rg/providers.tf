cc440e77beebc1732cd94073bbfeb7e634265097
JC Guerrero <JCystems@gMail.com>
2026-03-14 14:45:18 -0500

    feat: tflint (#37)

--- assets/terra/azurerm/v3/some-rg/providers.tf
+++ assets/terra/azurerm/v3/some-rg/providers.tf
@@ -1,12 +1,12 @@
 terraform {
+  required_version = "~> 1.14.3"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.106.1"
     }
   }
-
-  required_version = "~> 1.14.3"
 }
 
 provider "azurerm" {
