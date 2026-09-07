ee7895fa906ddba98a5a191b7cbf7fe7b4ee4cd2
percebus <JCystems@GMail.com>
2026-09-07 18:41:36 -0500

    aa

--- assets/terra/azurerm/v3/some-rg/providers.tf
+++ assets/terra/azurerm/v3/some-rg/providers.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_version = "~> 1.15.1"
-
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.106.1"
-    }
-  }
-}
 
 provider "azurerm" {
   features {}
