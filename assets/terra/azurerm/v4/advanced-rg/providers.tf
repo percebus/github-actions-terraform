f0a43258262587b1dce3ab7b6aa3bdf196b45d71
JC Guerrero <JCystems@gMail.com>
2026-09-07 19:47:24 -0500

    fix(`compliance`): version (#83)
    # Changes
    
    ## Issues
    
    - #84
    
    ## Summary
    
    Add `default` version to `1.15.1`

--- assets/terra/azurerm/v4/advanced-rg/providers.tf
+++ assets/terra/azurerm/v4/advanced-rg/providers.tf
@@ -1,19 +1,3 @@
-terraform {
-  required_version = "~> 1.15.1"
-
-  required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = "~>3.0"
-    }
-
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.64.0"
-    }
-  }
-
-}
 
 provider "azurerm" {
   features {}
---
+++ assets/terra/azurerm/v4/advanced-rg/terraform.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">= 1.15.1"
+
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.64.0"
+    }
+  }
+
+}
